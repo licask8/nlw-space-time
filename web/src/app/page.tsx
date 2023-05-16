fad8bb99e22c5ee17d4f36feee1718aaf2f9485f
licask8 <elielson.elrick@gmail.com>
2023-05-16 13:07:24 -0300

    aula 1

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900 text-green-200">
+      <h1 className="h-12 rounded bg-blue-700 p-3 text-2xl font-bold">
+        Sua capsula do tempo
+      </h1>
+    </div>
+  )
+}
